e8710ba840f8f6f8f716b6a85f9ecd0774e5db68
Ben Trachtenberg <e_ben_75-python@yahoo.com>
2026-07-21 17:43:18 -0500

    added more methods for projects

--- pkg/projects/projects.go
+++ pkg/projects/projects.go
@@ -4,6 +4,8 @@ Package projects provides a way to manipulate projects for Ansible AAP
 package projects
 
 import (
+	"fmt"
+
 	"github.com/btr1975/go-ansible-aap-api-client/pkg/connection"
 	"github.com/btr1975/go-ansible-aap-api-client/pkg/dataconversion"
 )
@@ -43,4 +45,48 @@ func (project *Project) GetAllProjects() (schemaResponse ProjectResponseSchema, 
 	}
 
 	return schemaResponse, nil
+}
+
+// GetProject gets a project by name
+//
+//	:param name: The name of the project to get
+func (project *Project) GetProject(name string) (schemaResponse ProjectResponseSchema, err error) {
+	schemaResponse = ProjectResponseSchema{}
+
+	params := map[string]string{
+		"name": name,
+	}
+
+	response, err := project.connection.Get(project.URI, params)
+
+	if err != nil {
+		return schemaResponse, err
+	}
+
+	err = project.DataConversion.ResponseBodyToStruct(&schemaResponse, *response)
+
+	if err != nil {
+		return schemaResponse, err
+	}
+
+	return schemaResponse, nil
+}
+
+// GetProjectID gets a job template ID by name
+//
+//	:param name: The name of the project to get the ID for
+func (project *Project) GetProjectID(name string) (id int32, err error) {
+	schemaResponse, err := project.GetProject(name)
+
+	if err != nil {
+		return 0, err
+	}
+
+	if len(schemaResponse.Results) == 0 {
+		return 0, fmt.Errorf("no project found with name %s", name)
+	} else if len(schemaResponse.Results) > 1 {
+		return 0, fmt.Errorf("more than one project found with name %s", name)
+	}
+
+	return schemaResponse.Results[0].ID, nil
 }
