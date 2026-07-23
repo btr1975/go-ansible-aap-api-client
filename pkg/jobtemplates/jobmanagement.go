bbd991ce5496f22891cd36ceb5e683eb4ed95c50
Ben Trachtenberg <e_ben_75-python@yahoo.com>
2026-07-23 13:27:50 -0500

    refactored polling launc to respond with a struct for cleaner usage

--- pkg/jobtemplates/jobmanagement.go
+++ pkg/jobtemplates/jobmanagement.go
@@ -78,41 +78,41 @@ func (jobManagement *JobManagement) Run(launchData JobTemplateSimpleRequestSchem
 func (jobManagement *JobManagement) PollCompletion(
 	printStatus bool,
 	launchData JobTemplateSimpleRequestSchema,
-) (jobStatus string, err error) {
-	jobStatus = "new"
+) (completionResponse JobManagementPollCompletionResponseSchema, err error) {
+	response := JobManagementPollCompletionResponseSchema{Status: "new", JobID: jobManagement.jobID}
 
 	if jobManagement.jobID == 0 {
 
 		err = jobManagement.Run(launchData)
 
+		response.JobID = jobManagement.jobID
+
 		if err != nil {
-			return jobStatus, err
+			return response, err
 		}
 	}
 
 	if printStatus {
-		fmt.Printf("Polling Job ID %d current status %s\n", jobManagement.jobID, jobStatus)
+		fmt.Printf("Polling Job ID %d current status %s\n", response.JobID, response.Status)
 	}
 
-	for jobStatus != "successful" && jobStatus != "failed" && jobStatus != "error" && jobStatus != "cancelled" {
-		currentStatus, err := jobManagement.job.GetJobStatus(jobManagement.jobID)
+	for response.Status != "successful" && response.Status != "failed" && response.Status != "error" && response.Status != "cancelled" {
+		response.Status, err = jobManagement.job.GetJobStatus(response.JobID)
 
 		if err != nil {
-			return jobStatus, err
+			return response, err
 		}
 
-		jobStatus = currentStatus
-
 		if printStatus {
-			fmt.Printf("Polling Job ID %d current status %s\n", jobManagement.jobID, jobStatus)
+			fmt.Printf("Polling Job ID %d current status %s\n", response.JobID, response.Status)
 		}
 
 		time.Sleep(5 * time.Second)
 	}
 
 	if printStatus {
-		fmt.Printf("Polling Job ID %d completed status %s\n", jobManagement.jobID, jobStatus)
+		fmt.Printf("Polling Job ID %d completed status %s\n", response.JobID, response.Status)
 	}
 
-	return jobStatus, nil
+	return response, nil
 }
